68951b058643ca593f7e165084f26666fb568818
Dimpfe <dimasprasetyo7696@gmail.com>
2024-05-23 16:17:11 +0700

    fix<>: fix the error handler

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,32 +1,42 @@
-const predictClassification = require("../service/inferenceService");
-const storeData = require("../service/storeData");
+const predictClassification = require('../service/inferenceService');
+const storeData = require('../service/storeData');
+const getHistoryData = require("../service/getHistoryData");
 
-const postPredictionHandler = async(request, h) => {
+const crypto = require('crypto');
+
+async function postPredictionHandler(request, h) {
    const { image } = request.payload;
    const { model } = request.server.app;
 
-   const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
-
+   const { label } = await predictClassification(model, image);
    const id = crypto.randomUUID();
    const createdAt = new Date().toISOString();
 
    const data = {
       "id": id,
       "result": label,
-      "explanation": explanation,
-      "suggestion": suggestion,
-      "confidenceScore": confidenceScore,
-      "createdAt": createdAt,
-   };
-   storeData(id, data);
+      "suggestion": label == 'Cancer' ? 'Segera konsultasi ke dokter!' : 'Tidak perlu khawatir...',
+      "createdAt": createdAt
+   }
+
+   await storeData(id, data);
 
    const response = h.response({
       status: 'success',
-      message: confidenceScore > 99 ? 'Model Trained Successfully' : 'Model predicted successfully but under threshold',
+      message: 'Model is predicted successfully',
       data
    })
    response.code(201);
    return response;
 }
 
-module.exports = postPredictionHandler;
+async function getHistoryHandler(_request, h) {
+   histories = await getHistoryData()
+   const response = h.response({
+      status: 'success',
+      data: histories
+   })
+   return response;
+}
+
+module.exports = {postPredictionHandler, getHistoryHandler};
